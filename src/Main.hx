35c64b87380834cb960ddb68d8902458a43f30b6
Patric Burgherr <IceOPede@users.noreply.github.com>
2019-07-09 11:03:31 +0200

    Camera movement

--- src/Main.hx
+++ src/Main.hx
@@ -15,7 +15,7 @@ class Main extends hxd.App {
 	var cameraRight:Vector;
 
 	override function init() {
-		this.cameraPos = new Vector(0.0, -10.0, 0.0);
+		this.cameraPos = new Vector(0.0, 0.0, 0.0);
 		this.cameraTarget = new Vector(0.0, 1.0, 0.0);
 
 		this.cameraDirection = this.cameraPos.sub(this.cameraTarget);
@@ -28,7 +28,7 @@ class Main extends hxd.App {
 
 		s3d.camera.pos = cameraPos;
 		s3d.camera.up = cameraUp;
-		s3d.camera.target = cameraTarget;
+		s3d.camera.target = cameraPos.add(cameraTarget);
 
 		//
 
@@ -41,7 +41,7 @@ class Main extends hxd.App {
 		var obj2 = new Mesh(prim, s3d);
 		obj2.material.color.setColor(0xFFB280);
 		obj2.x = 0;
-		obj2.y = 0;
+		obj2.y = 10.0;
 		obj2.z = 0;
 		obj2.scale(0.6);
 		obj2.material.shadows = false;
@@ -49,7 +49,34 @@ class Main extends hxd.App {
 	}
 
 	override function update(dt:Float) {
-		
+		var cameraSpeed = 2.5 * dt;
+
+		if (hxd.Key.isDown(hxd.Key.W)) {
+			var temp = cameraTarget.clone();
+			temp.scale3(cameraSpeed);
+			cameraPos = cameraPos.add(temp);
+		}
+		if (hxd.Key.isDown(hxd.Key.S)) {
+			var temp = cameraTarget.clone();
+			temp.scale3(cameraSpeed);
+			cameraPos = cameraPos.sub(temp);
+		}
+		if (hxd.Key.isDown(hxd.Key.A)) {
+			var temp = cameraTarget.cross(cameraUp);
+			temp.normalize();
+			temp.scale3(cameraSpeed);
+			cameraPos = cameraPos.add(temp);
+		}
+		if (hxd.Key.isDown(hxd.Key.D)) {
+			var temp = cameraTarget.cross(cameraUp);
+			temp.normalize();
+			temp.scale3(cameraSpeed);
+			cameraPos = cameraPos.sub(temp);
+		}
+
+		s3d.camera.pos = cameraPos;
+		s3d.camera.up = cameraUp;
+		s3d.camera.target = cameraPos.add(cameraTarget);
 	}
 
 	static function main() {
